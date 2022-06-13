4308a71ac95f4ec1b93e2459b54e8712684f22c6
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-14 00:05:10 +0300

    Add Banner Component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['links.papareact.com'],
+//   },
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: ['links.papareact.com'],
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <title>AIRBNB CLONE</title>
       </Head>
       <Header />
+      <Banner />
     </div>
   );
 }
